e00ea7f05e30b94affda7522daf6c159c23125e7
maccabee0 <maccabeeodin@Gmail.com>
2014-08-04 17:56:33 +0300

    missed

--- Corporate/Corporate.WPF/App.xaml.cs
+++ Corporate/Corporate.WPF/App.xaml.cs
@@ -15,8 +15,35 @@ namespace Corporate.WPF
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+#if (DEBUG)
+            RunInDebugMode();
+#else
+            RunInReleaseMode();
+#endif
+        }
+
+        private static void RunInDebugMode()
+        {
             var bootstrapper = new CorporateBootstrapper();
             bootstrapper.Run();
+        }
+
+        private static void RunInReleaseMode()
+        {
+            AppDomain.CurrentDomain.UnhandledException += AppDomainUnhandledException;
+        }
+
+        private static void AppDomainUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            UnhandledException(e.ExceptionObject as Exception);
+        }
+
+        private static void UnhandledException(Exception ex)
+        {
+            if(ex==null)
+                return;
+
+            //ExceptionPolicy.H
         }
     }
 }
